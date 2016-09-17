bcf5a510840ba7f4762c78fb058be4560ee92e2f
Bittuw <bittumworker@mail.ru>
2016-09-17 12:02:11 +0300

    Add new files

---
+++ Лаба 2/arr_args.py
@@ -0,0 +1,21 @@
+﻿def min(arr):
+	min = arr[0]
+	for i in arr:
+		if min > i:
+			min = i
+	return min
+
+def avg(arr):
+	avg = 0
+	for i in arr:
+		avg = avg + i
+	avg = avg/len(arr)
+	return avg
+
+if __name__ == "__main__":
+	arr = [1,2,3,4,5]
+	print(arr)
+	print('Минимум ' + str(min(arr)))
+	print('Среднее арфиметическое ' + str(avg(arr)))
+
+
