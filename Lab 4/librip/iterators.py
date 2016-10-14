30ae63d26d04fb6e657e7da817dc5f0d64069e7a
Bittuw <bittumworker@mail.ru>
2016-10-14 22:16:55 +0300

    Сделано

--- Lab 4/librip/iterators.py
+++ Lab 4/librip/iterators.py
@@ -3,14 +3,14 @@ class Unique(object):
 	def __init__(self, items, **kwargs):
 		self.ignore_case = kwargs.get('ignore_case') is not None and kwargs.get('ignore_case')
 		self.items = iter(items)
-		self.seen = set()
+		self.set = set()
 
 	def __next__(self):
 		while True:
 			temp = self.items.__next__()
 			temp_val = str(temp).lower() if self.ignore_case else temp
-			if temp_val not in self.seen:
-				self.seen.add(temp_val)
+			if temp_val not in self.set:
+				self.set.add(temp_val)
 				return temp
 	def __iter__(self):
 		return self
